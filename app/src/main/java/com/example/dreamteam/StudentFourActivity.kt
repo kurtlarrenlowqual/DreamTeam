8f1931395a8495b35ae8fde8e65a95f95000b5e8
Joemrich <joemrichsalcedo@gmail.com>
2026-01-18 22:15:59 +0800

    Student Four

--- app/src/main/java/com/example/dreamteam/StudentFourActivity.kt
+++ app/src/main/java/com/example/dreamteam/StudentFourActivity.kt
@@ -1,4 +1,18 @@
-package com.example.dreamteam;
+package com.example.dreamteam
 
-public class StudentFourActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
+
+class StudentFourActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_four)
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
 }
