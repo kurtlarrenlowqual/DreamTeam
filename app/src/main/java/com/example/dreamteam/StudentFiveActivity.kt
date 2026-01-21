b753ac47a55af774f2d4b3933d34e7c51b3a6817
Jay Em <jayem.mejia22@gmail.com>
2026-01-21 22:29:01 +0800

    Student 5 : MEJIA

--- app/src/main/java/com/example/dreamteam/StudentFiveActivity.kt
+++ app/src/main/java/com/example/dreamteam/StudentFiveActivity.kt
@@ -1,1 +1,18 @@
 package com.example.dreamteam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.widget.Button
+
+class StudentFiveActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_five)
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+}
